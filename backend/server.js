6608f9e5c550075a5063d8d75f70088831f77202
tavendqr <dartavenf@gmail.com>
2025-12-28 10:45:04 -0500

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import productRoutes from "./routes/product.route.js";
@@ -8,6 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'https://mern-product-store-frontend-09i6.onrender.com',
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("API is running");
